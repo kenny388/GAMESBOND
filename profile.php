be50cde28796acf93c7319796dcaded4c374cec2
Unknown <kennycheung388@yahoo.com.hk>
2018-12-02 14:14:09 -0800

    Basic User Panel

--- profile.php
+++ profile.php
@@ -47,44 +47,59 @@
 	// $modelName = str_replace('%20', ' ', $receivedName);
 	include 'private/db_credentials.php';
 
-	if(isset($_SESSION['loggedIn'])) {
-		$user_id = $_SESSION['email'];
-			$query = "SELECT * FROM browse_History INNER JOIN games ON browse_History.game_id = games.FIELD1 WHERE user_id = '{$user_id}'";
-			$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-              if(mysqli_connect_errno()) {
-                $msg = "Database connection failed: ";
-                $msg .= mysqli_connect_error();
-                $msg .= " (" . mysqli_connect_errno() . ")";
-                exit($msg);
-              }
+	$user = $_SESSION["email"];
+	$query = "SELECT * FROM users JOIN rating ON users.email = rating.user_id WHERE user_id = '{$user}'";
+	$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+						if(mysqli_connect_errno()) {
+							$msg = "Database connection failed: ";
+							$msg .= mysqli_connect_error();
+							$msg .= " (" . mysqli_connect_errno() . ")";
+							exit($msg);
+						}
 
-			$result = $connection->query($query);
+	$result = $connection->query($query);
 
+	$commentCount = 0;
 
-			while ($row = @mysqli_fetch_assoc($result)) {
+	while ($row = @mysqli_fetch_assoc($result)) {
+		// $retrievedID = $row['FIELD1'];
+		// $retrievedName = $row['title'];
+		$userID = $row["user_id"];
+		$userFirstName = $row["first_name"];
+		$userLastName = $row["last_name"];
+		$commentCount += 1;
+		// $userCommentNumber = $row["count(*)"];
+		// echo $userCommentNumber;
 
-				$retrievedID = $row['FIELD1'];
-				$retrievedName = $row['title'];
-
-				echo "<a href=gameSpecific.php?gameCode=".$retrievedID . ">" . $retrievedName . "</a>";
-				echo "<br>";
-				echo '<img src="' . $row["images"] . '">';
-				echo "<br>";
-				echo $row['genre'];
-				echo "<br>";
-				echo "<br>";
-
-			}
-
-	} else {
-		echo "look at some games";
-		echo "<br>";
 	}
-		?>
+
+	// echo $commentCount;
+	?>
+
+	<div class="userPanel">
+		<div class="userBox">
+			<div class="profileImage">
+			<img src="img/KirbySquare.png"></img>
+		</div>
+		<div class="userName">
+
+			</div>
+		</div>
+		<div class="infoBox">
+						<h3><?php echo $userFirstName . " " . $userLastName; ?></h3>
+						<br>
+						<label>Email:</label>
+						<p><?php echo $userID; ?></p>
+						<br>
+						<label>Number of Games Rated</label>
+						<p><?php echo $commentCount; ?></p>
+						<br>
+		</div>
+	</div>
 
 		<?php
 		$user = $_SESSION["email"];
-		$query = "SELECT * FROM rating WHERE user_id = '{$user}'";
+		$query = "SELECT * FROM users JOIN rating ON users.email = rating.user_id WHERE user_id = '{$user}'";
 		$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
               if(mysqli_connect_errno()) {
                 $msg = "Database connection failed: ";
@@ -125,6 +140,55 @@
 
 		<!-- games ID -->
 		Games:
+
+
+			<div class="featuredTitle">
+					<h3>Browse History</h3>
+					<hr>
+			</div>
+
+		<div class="section">
+		<div class="gameSection">
+			<div class="GamesContainer">
+
+		<?php
+
+
+		if(isset($_SESSION['loggedIn'])) {
+			$user_id = $_SESSION['email'];
+				$query = "SELECT * FROM browse_History INNER JOIN games ON browse_History.game_id = games.FIELD1 WHERE user_id = '{$user_id}'";
+				$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+								if(mysqli_connect_errno()) {
+									$msg = "Database connection failed: ";
+									$msg .= mysqli_connect_error();
+									$msg .= " (" . mysqli_connect_errno() . ")";
+									exit($msg);
+								}
+
+				$result = $connection->query($query);
+
+				while ($row = @mysqli_fetch_assoc($result)) {
+
+					echo '<div class="gameEntry">';
+					echo '<a href="gameSpecific.php?gameCode=' . $row['FIELD1'] . '">';
+					echo '<div class="gameImages" style="background-image:url(' . $row["images"] . ')"></div>';
+					echo '</a>';
+					echo '<div class="gameNames">' . $row["title"] . '</div>';
+					echo '<div class="gameGenre">' . $row["time_stamp"] . '</div>';
+					echo '</div>';
+
+				}
+
+		} else {
+
+		}
+
+			?>
+
+		</div>
+		</div>
+
+	</div>
 
 	</div>
 </div>
